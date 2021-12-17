9431bce9ff32ce970963b95588828d7ee838cca5
Haruna Popoola <haruna.popoola@yahoo.com>
2021-12-17 10:31:55 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+// foo
+
+// toDO
+
+/*
+ todo
+ ToDo
+ */
+
+// TODO
+
+// TODO Explenation
+
+// [TODO]
+
+// PreTodo
+// toDomain
+
+/*
+ * TODO: Get rid of this multiline comment.
+ */
+
+console.log("Hello Todo App");
